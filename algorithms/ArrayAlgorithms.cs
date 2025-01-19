d1024cd57f2dd4258d743e69c4ddfe1515174a97
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-01-18 18:41:53 -0800

    Implemented the valid subsequence problem

---
+++ algorithms/ArrayAlgorithms.cs
@@ -0,0 +1,17 @@
+namespace basics.algorithms {
+    public class ArrayAlgorithms {
+        public static bool IsSubSequence(List<int> array, List<int> sequence) {
+            // start at 0
+            int idx = 0;
+            for (int i = 0; i < array.Count; i++) {
+                if (array[i].Equals(sequence[idx])) {
+                    idx++;
+                    if (idx.Equals(sequence.Count)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
