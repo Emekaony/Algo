18210395228d761c7988d5cd01e66ef41a36e21a
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-01-19 21:01:07 -0800

    reading json works

--- Program.cs
+++ Program.cs
@@ -4,12 +4,7 @@ using basics.utils;
 namespace basics {
     public class Program {
         public static void Main() {
-            List<int> array = [1, 2, 3, 4, 5];
-            List<int> sequence = [2, 3, 4];
-            bool verdict = ArrayAlgorithms.IsSubSequence(array, sequence);
-            if (verdict) {
-                Console.WriteLine($"{ArrayUtils.PrettyPrintArray(sequence)} is a valid subsequence of {ArrayUtils.PrettyPrintArray(array)}");
-            }
+            dataprocc.ReadBasicJson.ReadJson();
         }
     }
 }
