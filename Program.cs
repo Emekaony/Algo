2b53c256ce5775f949b98ae8bb0b53cabaaea70f
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-01-25 10:50:56 -0800

    Starting again

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿using basics.models;
-
-namespace basics {
-    public class Program {
-        public static void Main() {
-            Person pp = new("Nnaemeka", "Onyeokoro");
-            Console.WriteLine(pp.FullName);
-        }
-    }
-}
+﻿Console.WriteLine("Hello World");
