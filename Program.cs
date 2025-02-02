2b3da4be00f3306873c8f74c8fda340e29d72f62
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-02-02 01:01:45 -0800

    Working on some fundamental string algorithms

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello World");
+﻿using basics.algorithms.stringAlgorithms;
+
+namespace basics {
+    public class Program() {
+        public static void Main() {
+            string word = "emeka";
+            string reversedWord = Fundamentals.ReverseWord(word);
+            Console.WriteLine(reversedWord);
+        }
+    }
+}
---
+++ algorithms/stringAlgorithms/Fundamentals.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using System.Text;
+
+namespace basics.algorithms.stringAlgorithms {
+    public class Fundamentals() {
+        public static bool IsUppercase(string word) {
+            return word.All(char.IsUpper);
+        }
+
+        public static string ReverseWord(string word) {
+            // just return the string if it's null or empty
+            if (string.IsNullOrEmpty(word)) {
+                return word;
+            }
+
+            // give stringBuilder length for optimization
+            StringBuilder sb = new(word.Length);
+            for (int i = word.Length - 1; i >= 0; i--) {
+                sb.Append(word[i]);
+            }
+
+            // get the string from the stringBuilder
+            return sb.ToString();
+        }
+    }
+}
